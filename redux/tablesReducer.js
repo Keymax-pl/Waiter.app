94764d34c4cf293f9bc2b5ac4b7c2a26b7d43f9c
Karol Kropisz <karolkropisz@gmail.com>
2024-02-05 18:48:21 +0100

    Initial project

---
+++ redux/tablesReducer.js
@@ -0,0 +1,17 @@
+
+
+// Selectors
+
+// Actions
+const createActionName = actionName => 'app/tables/${actionName';
+
+
+// Actions creators
+const tablesReducer = (statePart = [], action) => {
+  switch (action.type) {
+    default:
+        return statePart;
+  };
+};
+
+export default tablesReducer;
